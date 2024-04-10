b389db85e7dedb3f7efdf1b41de79bd9063ec3da
Kinga Zawarty <niu.ula.a@gmail.com>
2024-04-10 20:24:08 +0200

    added cloud

--- lilac/src/App.js
+++ lilac/src/App.js
@@ -5,10 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          orichidea lilac
-          kinga zawarty
+        <p className="main-cloud">
+          <img src={logo} className="App-logo" alt="logo" /> <br></br>
+          kinga zawarty <br></br>
+          <a href="https://containedx.itch.io/interactive-resume" target="_blank">my interactive resume</a> <br></br>
+          <a href="https://www.linkedin.com/in/kinga-zawarty/" target="_blank">linkedin</a> <br></br>
         </p>
       </header>
     </div>
