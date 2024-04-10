f30ef89d1b2a3c7b81787e5f69c08595c0b6fc69
Kinga Zawarty <niu.ula.a@gmail.com>
2024-04-10 20:40:02 +0200

    adding avatar, style improvements

--- lilac/src/App.js
+++ lilac/src/App.js
@@ -1,4 +1,5 @@
 import logo from './assets/cat_pixel.png';
+import avatar from './assets/my_pixel_avatar.png'
 import './App.css';
 
 function App() {
@@ -6,10 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p className="main-cloud">
+
+          <p className="title"> kinga zawarty </p>
+
+          <img src={avatar} className="avatar" />
           <img src={logo} className="App-logo" alt="logo" /> <br></br>
-          kinga zawarty <br></br>
+
+
           <a href="https://containedx.itch.io/interactive-resume" target="_blank">my interactive resume</a> <br></br>
           <a href="https://www.linkedin.com/in/kinga-zawarty/" target="_blank">linkedin</a> <br></br>
+          <a href="https://github.com/containedx" target="_blank">github</a> <br></br>
+
         </p>
       </header>
     </div>
