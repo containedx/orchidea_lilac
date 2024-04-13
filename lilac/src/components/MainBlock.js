4e6fb081e4836892ed98bbb88645c5fa98de03eb
Kinga Zawarty <niu.ula.a@gmail.com>
2024-04-13 16:11:09 +0200

    more interactivity, refactoring

--- lilac/src/components/MainBlock.js
+++ lilac/src/components/MainBlock.js
@@ -1,6 +1,6 @@
 
 import './MainBlock.css';
-import logo from '../assets/cat_pixel.png';
+import cat from '../assets/cat_pixel.png';
 import avatar from '../assets/my_pixel_avatar.png';
 import avatar2 from '../assets/my_pixel_avatar2.png';
 import React, { useState } from 'react';
@@ -14,16 +14,18 @@ export default function MainBlock() {
 
             <p className="title"> kinga zawarty </p>
 
-            <img src={hovered ? avatar2 : avatar} className="avatar"
-                onMouseEnter={() => setHovered(true)}
-                onMouseLeave={() => setHovered(false)} />
-            <img src={logo} className="App-logo" alt="logo" /> <br></br>
+            <div>
+                <img src={hovered ? avatar2 : avatar} className="avatar"
+                    onMouseEnter={() => setHovered(true)}
+                    onMouseLeave={() => setHovered(false)} />
+                <img src={cat} className="cat" /> <br></br>
+            </div>
 
-
-            <a href="https://containedx.itch.io/interactive-resume" target="_blank">my interactive resume</a> <br></br>
-            <a href="https://www.linkedin.com/in/kinga-zawarty/" target="_blank">linkedin</a> <br></br>
-            <a href="https://github.com/containedx" target="_blank">github</a> <br></br>
-
+            <div className="links">
+                <a href="https://containedx.itch.io/interactive-resume" target="_blank">my interactive resume</a>
+                <a href="https://www.linkedin.com/in/kinga-zawarty/" target="_blank">linkedin</a>
+                <a href="https://github.com/containedx" target="_blank">github</a>
+            </div>
         </div>
     );
 
