0e9ec90f8ad966b181b86cb21d5c3f4b750be844
Kinga Zawarty <niu.ula.a@gmail.com>
2024-04-13 15:39:36 +0200

    use state, change photo on hover

--- lilac/src/components/MainBlock.js
+++ lilac/src/components/MainBlock.js
@@ -1,16 +1,22 @@
 
 import './MainBlock.css';
 import logo from '../assets/cat_pixel.png';
-import avatar from '../assets/my_pixel_avatar.png'
+import avatar from '../assets/my_pixel_avatar.png';
+import avatar2 from '../assets/my_pixel_avatar2.png';
+import React, { useState } from 'react';
 
 export default function MainBlock() {
+
+    const [hovered, setHovered] = useState(false);
 
     return (
         <div className="main-cloud">
 
             <p className="title"> kinga zawarty </p>
 
-            <img src={avatar} className="avatar" />
+            <img src={hovered ? avatar2 : avatar} className="avatar"
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)} />
             <img src={logo} className="App-logo" alt="logo" /> <br></br>
 
 
